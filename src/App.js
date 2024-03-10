d509bef65e72ab6b8bd8cd9fe46b10e549f6638e
Saimon <saimon22011@gmail.com>
2024-03-10 19:32:00 +0400

    unfinished wrapper layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import {Component} from "react";
+
+
 import './App.css';
+
+class Wrapper extends Component{
+  render() {
+    return(
+        <div id="wrapper">
+          <div id="quote-box">
+            <div className="quote-text">
+              <i className="fa fa-quote-left"></i>
+              <span id="text"></span>
+            </div>
+            <div className="quote-author">
+              <span id="author"></span>
+            </div>
+            <div className="buttons">
+                <a className="button" id="tweet-quote" target="_top">
+                    <i className="fa fa-twitter"></i>
+                </a>
+                <a className="button" id="tumblr-quote" target="_top">
+                    <i className="fa fa-tumblr"></i>
+                </a>
+                <button className="button" id="new-quote"></button>
+            </div>
+          </div>
+        </div>
+    )
+  }
+}
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Wrapper/>
     </div>
   );
 }
