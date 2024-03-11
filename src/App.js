974a3dee43b2b7d998d9cf34b6b6ecc02964d95b
Saimon <saimon22011@gmail.com>
2024-03-11 12:26:53 +0400

    feat: added fetch for quotes

--- src/App.js
+++ src/App.js
@@ -5,18 +5,50 @@ import './App.css';
 
 class Wrapper extends Component{
 
+constructor(props) {
+  super(props);
+  this.state={
+    data:null,
+    error:null
+  };
+}
 
+componentDidMount() {
+  this.fetchData();
+}
+
+async fetchData(){
+  try{
+    const response = await fetch('https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json')
+    if (!response.ok) {
+      throw new Error('Error during data loading');
+    }
+    const json = await response.json();
+
+    this.setState({data: json.quotes})
+
+  }catch (error){
+    this.setState({error:error.message})
+  }
+}
 
   render() {
+    const { data, error } = this.state;
+    let index = 0
+
     return(
         <div id="wrapper">
           <div id="quote-box">
             <div className="quote-text">
               <i className="fa fa-quote-left"></i>
-              <span id="text"></span>
+              {error && <span id="text">Error:{error}</span>}
+              {data && <span id="text">{data[index].quote}</span>}
             </div>
             <div className="quote-author">
-              <span id="author"></span>
+              {error && <span id="author">Error:{error}</span>}
+              {data && <span id="author">- {data[index].author}</span>}
+
+
             </div>
             <div className="buttons">
                 <a className="button" id="tweet-quote" target="_top">
