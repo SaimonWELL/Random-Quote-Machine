03a0e22b2349c63667e163ddce092bf2c1936dd3
Saimon <saimon22011@gmail.com>
2024-03-17 12:24:32 +0400

    feat: added smooth transition for background and text colour change .Added links for sharing quotes

--- src/App.js
+++ src/App.js
@@ -11,12 +11,30 @@ constructor(props) {
     data:null,
     error:null,
     index:0,
-    opacity:1
+    opacity:1,
+    indexColor:0,
+    loading:true
   };
 }
 
 componentDidMount() {
-  this.fetchData();
+    const colors = [
+        '#16a085',
+        '#27ae60',
+        '#2c3e50',
+        '#f39c12',
+        '#e74c3c',
+        '#9b59b6',
+        '#FB6964',
+        '#342224',
+        '#472E32',
+        '#BDBB99',
+        '#77B1A9',
+        '#73A857'
+    ];
+    const body = document.querySelector('body');
+    body.style.backgroundColor = colors[this.state.indexColor]
+    this.fetchData();
 }
 
 async fetchData(){
@@ -27,81 +45,110 @@ async fetchData(){
     }
     const json = await response.json();
 
-    this.setState({data: json.quotes})
+    this.setState({
+        data: json.quotes,
+        loading: false
+
+    })
 
   }catch (error){
     this.setState({error:error.message})
   }
 }
 
-newQuote = (min,max)=> {
-    min = Math.ceil(min);
+async newQuote (min,max,colors) {
+    min = Math.floor(min);
     max = Math.floor(max);
+
+    const body = document.querySelector('body');
+    const minColor = 0,
+          maxColor = colors.length
     this.setState({opacity: 0})
-    setTimeout(()=>{
-        this.setState(({index})=>{
-            return {
-                index: Math.floor(Math.random() * (max - min + 1)) + min
-            }
-        })
-    },500)
-    setTimeout(() => {
-        this.setState({ opacity: 1 });
-    }, 500);
+    await new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            this.setState(()=>{
+                return {
+                    index: Math.floor(Math.random() * (max - min + 1)) + min,
+                    opacity: 1,
+                    indexColor: Math.floor(Math.random() * (maxColor - minColor + 1)) + minColor,
+                }
+            })
+
+            body.style.backgroundColor = colors[this.state.indexColor]
+        },500);
+
+    })
 
 }
 
- async decreaseOpacity() {
-     this.setState({ opacity: 0 });
-     // Через определенный промежуток времени (например, 1 секунду) возвращаем прозрачность обратно к 1
-     setTimeout(() => {
-         this.setState({ opacity: 1 });
-     }, 2000);
-};
+
 
   render() {
-    const { data, error ,index, opacity} = this.state;
+    const { data, error ,index, opacity, indexColor,loading} = this.state;
 
-
+    const colors = [
+        '#16a085',
+        '#27ae60',
+        '#2c3e50',
+        '#f39c12',
+        '#e74c3c',
+        '#9b59b6',
+        '#FB6964',
+        '#342224',
+        '#472E32',
+        '#BDBB99',
+        '#77B1A9',
+        '#73A857'
+    ];
+    let tt ='asdasd'
 
     return(
         <div id="wrapper">
-          <div id="quote-box">
-            <div className="quote-text text-opacity" style={{opacity: opacity}}>
-              <i className="fa fa-quote-left"></i>
-              {error && <span id="text">Error:{error}</span>}
-              {data && <span id="text" className='' >{data[index].quote}</span>}
-            </div>
-            <div className="quote-author ">
-              {error && <span id="author">Error:{error}</span>}
-              {data && <span id="author">- {data[index].author}</span>}
-            </div>
-            <div className="buttons">
-                <a className="button" id="tweet-quote" target="_top">
-                    <i className="fa fa-twitter"></i>
-                </a>
-                <a className="button" id="tumblr-quote" target="_top">
-                    <i className="fa fa-tumblr"></i>
-                </a>
-                <button onClick={()=>{
-                    this.newQuote(0,data.length);
-                     }
-                } className="button " id="new-quote">New quote</button>
-            </div>
-          </div>
+            {loading ?(<p>Please wait</p>): (
+                <div id="quote-box">
+                    <div className="quote-text " style={{opacity: opacity, color: colors[indexColor]}}>
+                        <i className="fa fa-quote-left"></i>
+                        {error && <span id="text">Error:{error}</span>}
+                        {data && <span id="text">{data[index].quote}</span>}
+                    </div>
+                    <div className="quote-author " style={{opacity: opacity, color: colors[indexColor]}}>
+                        {error && <span id="author">Error:{error}</span>}
+                        {data && <span id="author">- {data[index].author}</span>}
+                    </div>
+                    <div className="buttons">
+                        <a href={`https://twitter.com/intent/tweet?text=${encodeURIComponent(data[index].quote)}`}
+                           className="button" id="tweet-quote" target="_top"
+                           style={{backgroundColor: colors[indexColor]}}>
+                            <i className="fa fa-twitter"></i>
+                        </a>
+                        <a href={`https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=start&caption=hello&content=${encodeURIComponent(data[index].quote)}&canonicalUrl=${encodeURIComponent('https://www.tumblr.com/buttons&shareSource=tumblr_share_button')}`} className="button" id="tumblr-quote" target="_top"
+                           style={{backgroundColor: colors[indexColor]}}>
+                            <i className="fa fa-tumblr"></i>
+                        </a>
+                        <button onClick={() => {
+                            this.newQuote(0, data.length, colors);
+                        }
+                        } className="button " id="new-quote" style={{backgroundColor: colors[indexColor]}}>New quote
+                        </button>
+                    </div>
+                </div>
+            )
+
+            }
+
         </div>
+
     )
   }
 }
 
 
-
 function App() {
-  return (
-    <div className="App">
-      <Wrapper/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Wrapper/>
+        </div>
+    );
 }
 
 export default App;
